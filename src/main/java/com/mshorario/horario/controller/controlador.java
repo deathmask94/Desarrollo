c477e049e8d4db2268ac1d847c33c61d1acd7056
deathmask94 <franci.ortegac@duocuc.cl>
2025-05-23 19:02:05 -0400

    Update controlador.java

--- src/main/java/com/mshorario/horario/controller/controlador.java
+++ src/main/java/com/mshorario/horario/controller/controlador.java
@@ -34,7 +34,7 @@ public class controlador {
         return servicio.crearHorario(horario);
     }
 
-    @DeleteMapping("/borrar")
+    @DeleteMapping("/borrar/{hora}")
     public ResponseEntity<String> eliminarPorHora(@PathVariable String hora) {
         servicio.eliminarPorHora(hora);
         return ResponseEntity.ok("Horario eliminado correctamente");
